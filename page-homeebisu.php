c4ed22ddb2533fedeb6c7eb2a373c144a09cd112
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-01-10 16:08:21 +0900

    feat/#19老人ホームサブメニュー作成及びスタイリング(pc版)

--- page-homeebisu.php
+++ page-homeebisu.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <main>
         <?php if( have_posts()): while( have_posts()): the_post(); ?>
-        <div class="inner max-w[90rem]">
-            <div class="max-w[90rem] u-justify-center u-disp-flex u-margin-mxa">
+        <div class="inner u-w_maxpc u-mar-mxa">
+            <div class="max-w[90rem] u-justify-center u-disp-flex u-mar-mxa u-mar-b45">
                 <h1>
                     <picture class="u-posi-rela u-disp-flex u-justify-center">
                         <source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_mv.png" media="(min-width: 900px)" >
@@ -11,17 +11,15 @@
                     </picture>
                 </h1>
             </div>
-        </div>
-        <nav>
-            <ul>
-                <li>
-                    <?php wp_nav_menu(
-                        array('theme_location' => 'homeebisu-menu')
-                    );
-                    ?>
-                </li>
-        </ul>
+            <nav class="u-disp-flex u-h_70 u-justify-end">
+            <?php wp_nav_menu(array(
+                'theme_location' => 'homeebisu-menu',
+                'menu_class' => 'p-homeebisu_menu',
+                'container' => '',
+                ));?>
         </nav>
+        </div>
+        
         <section>
             <?php the_content(); ?>
         </section>    
