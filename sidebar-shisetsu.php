38cc8abae86f7c2d9d9d17916f58cfeebfa57a39
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-01-12 15:59:51 +0900

    feat/#21 老人ホームサイドバー大まかなレスポンシブ対応完成

--- sidebar-shisetsu.php
+++ sidebar-shisetsu.php
@@ -1,16 +1,18 @@
 <!--施設紹介ページ用sidebar-->
-<aside class="p-sidebar-shisetsu u-w_30p u-mar-mxa u-pad-pl6">
-    <h3 clsdd="p-siderbar-title">施設紹介</h3>
-    <div class="p-sidebar-btn u-w_72p u-h_148 u-mar-b21">
-        <p class="info"><a href="<?php echo home_url('/homeebisu/'); ?>">有料老人ホーム笑びす</a></p>
-    </div>
-    <div class="p-sidebar-btn u-w_72p u-h_148 u-mar-b21">
-        <p class="form"><a href="<?php echo home_url('/yoroebisu/'); ?>">宅幼老所笑びす</a></p>
-    </div>
-    <div class="p-sidebar-btn u-w_72p u-h_148 u-mar-b21">
-        <p class="form"><a href="<?php echo home_url('/dayebisu/'); ?>">デイサービス笑びす</a></p>
-    </div>
-    <div class="p-sidebar-btn u-w_72p u-h_148">
-        <p class="form"><a href="<?php echo home_url('/daykutsuku/'); ?>">デイサービス久津具</a></p>
-    </div>
+<aside class="p-sidebar-shisetsu u-w_30p85p u-mar-mxa u-pad-pl6">
+    <h3 class="p-sidebar-title">施設紹介</h3>
+    <ul class="p-sidebar-wrap">
+        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
+            <p class="info"><a href="<?php echo home_url('/homeebisu/'); ?>">有料老人ホーム笑びす</a></p>
+        </li>
+        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
+            <p class="form"><a href="<?php echo home_url('/yoroebisu/'); ?>">宅幼老所笑びす</a></p>
+        </li>
+        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
+            <p class="form"><a href="<?php echo home_url('/dayebisu/'); ?>">デイサービス笑びす</a></p>
+        </li>
+        <li class="p-sidebar-btn u-w_72p40p">
+            <p class="form"><a href="<?php echo home_url('/daykutsuku/'); ?>">デイサービス久津具</a></p>
+        </li>
+    </ul>
 </aside>
