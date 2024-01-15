38c5d049023b353990e6fd22930b7cff2684ec7d
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-01-15 16:11:15 +0900

    feat/#21サイドバー及びfeat/#23サブメニュースタイリング

---
+++ archive-home.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+    <main>
+        
+        <div class="inner u-w_maxpc u-mar-mxa">
+
+            <!--メインビジュアル-->
+            <div class="max-w[90rem] u-justify-center u-disp-flex u-mar-mxa u-mar-b45">
+                <h1>
+                    <picture class="u-posi-rela u-disp-flex u-justify-center">
+                        <source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_mv.png" media="(min-width: 200px)" >
+                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/page_mu.png" alt="">
+                        <p class="u-posi-abso c-title_mv">有料老人ホーム<br><span>笑びすについて</span></p>
+                    </picture>
+                </h1>
+            </div>
+
+           
+            <!--コンテンツ-->
+            <div class="p-homeebisu_main u-mar-mx1p">
+
+                <div class="p-homeebisu_content u-w_100p70p">
+                    <!--ページ内リンク用サブメニュー-->
+                    <nav class="p-nav-wrap u-h_100p">
+                        <?php wp_nav_menu(array(
+                            'theme_location' => 'homeebisu-menu',
+                            'menu_class' => 'p-nav-home',
+                            'container' => '',
+                            ));?>
+                    </nav>
+
+                    <!--メインコンテンツ-->
+                    <section class="u-w_70p">
+                        <?php if( have_posts()) : while( have_posts()) : the_post(); ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <?php the_content(); ?>
+                            </article>
+                        <?php endwhile; else : ?>
+                            <p>記事はありません。</p>
+                        <?php endif; ?>
+                    </section> 
+                </div>
+                
+                <!--サイドバー-->
+                <?php get_sidebar('shisetsu'); ?>
+                
+            </div>
+
+        </div> 
+        
+    </main>
+    
+<?php get_footer(); ?>
--- sidebar-shisetsu.php
+++ sidebar-shisetsu.php
@@ -2,8 +2,12 @@
 <aside class="p-sidebar-shisetsu u-w_30p85p u-mar-mxa u-pad-pl6">
     <h3 class="p-sidebar-title">施設紹介</h3>
     <ul class="p-sidebar-wrap">
-        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
-            <p class="info"><a href="<?php echo home_url('/homeebisu/'); ?>">有料老人ホーム笑びす</a></p>
+        <li class="p-sidebar-btn p-sidebar-bk p-sidebar-icon1 u-w_72p40p u-mar-b21">
+            <p class="info">
+                <a href="<?php echo home_url('/homeebisu/'); ?>">  
+                有料老人ホーム笑びす
+                </a>
+            </p>
         </li>
         <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
             <p class="form"><a href="<?php echo home_url('/yoroebisu/'); ?>">宅幼老所笑びす</a></p>
