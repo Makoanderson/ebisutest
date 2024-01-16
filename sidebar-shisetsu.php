06f38d431614f27f64b4b304bcbc7e5a3378325b
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-01-16 16:09:04 +0900

    feat/#21老人ホームサイドバースタイリング

--- sidebar-shisetsu.php
+++ sidebar-shisetsu.php
@@ -2,21 +2,17 @@
 <aside class="p-sidebar-shisetsu u-w_30p85p u-mar-mxa u-pad-pl6">
     <h3 class="p-sidebar-title">施設紹介</h3>
     <ul class="p-sidebar-wrap">
-        <li class="p-sidebar-btn p-sidebar-bk p-sidebar-icon1 u-w_72p40p u-mar-b21">
-            <p class="info">
-                <a href="<?php echo home_url('/homeebisu/'); ?>">  
-                有料老人ホーム笑びす
-                </a>
-            </p>
+        <li class="p-sidebar-btn c-bk_sidebar c-bk_sidebar1 p-sidebar-icon1 u-w_72p40p u-mar-b21">
+            <p class="c-btn_barfont"><a href="<?php echo home_url('/homeebisu/'); ?>">有料老人ホーム<br><span>笑びす</span></a></p>
         </li>
-        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
-            <p class="form"><a href="<?php echo home_url('/yoroebisu/'); ?>">宅幼老所笑びす</a></p>
+        <li class="p-sidebar-btn p-sidebar-bk c-bk_sidebar c-bk_sidebar2 p-sidebar-icon2 u-w_72p40p u-mar-b21">
+            <p class="form c-btn_barfont"><a href="<?php echo home_url('/yoroebisu/'); ?>">宅幼老所<br><span>笑びす</span></a></p>
         </li>
-        <li class="p-sidebar-btn u-w_72p40p u-mar-b21">
-            <p class="form"><a href="<?php echo home_url('/dayebisu/'); ?>">デイサービス笑びす</a></p>
+        <li class="p-sidebar-btn p-sidebar-bk c-bk_sidebar c-bk_sidebar3 p-sidebar-icon3  u-w_72p40p u-mar-b21">
+            <p class="form c-btn_barfont"><a href="<?php echo home_url('/dayebisu/'); ?>">デイサービス<br><span>笑びす</span></a></p>
         </li>
-        <li class="p-sidebar-btn u-w_72p40p">
-            <p class="form"><a href="<?php echo home_url('/daykutsuku/'); ?>">デイサービス久津具</a></p>
+        <li class="p-sidebar-btn p-sidebar-bk c-bk_sidebar c-bk_sidebar4 p-sidebar-icon4 u-w_72p40p">
+            <p class="form c-btn_barfont"><a href="<?php echo home_url('/daykutsuku/'); ?>">デイサービス<br><span>久津具</span></a></p>
         </li>
     </ul>
 </aside>
