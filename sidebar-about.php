eeef31c231a89278ef10b2007aa406925ede94fb
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-02-05 15:58:14 +0900

    feat/#28 みつわサイドバー95%程完成, #47みつわコンテンツスタイリング

--- sidebar-about.php
+++ sidebar-about.php
@@ -1,23 +1,26 @@
 <!--笑びすaboutページ用sidebar-->
-<aside class="sidebar-about u-w_30p85p u-mar-mxa u-pad-pl6 u-pad-pt12">
-    <ul class="">
-        <li class="sidebar-btn u-w_sbar u-mar-b21 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec1/'); ?>">コンセプト</a></p>
+<aside class="sidebar-about u-w_30p85p u-mar-mxa u-pad-pl6 u-pad-pt12pt0 c-bk_sidebk">
+    <ul class="p-sidebar_about">
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="info c-btn_barfont2"><a href="#m-concept">コンセプト</a></p>
         </li>
-        <li class="sidebar-btn u-w_sbar u-mar-b21 p-sidebar_btnline c-bk_sidebar5">
-            <p class="form c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec2/'); ?>">みつわの目的</a></p>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="form c-btn_barfont2"><a href="#m-purpose">みつわの目的</a></p>
         </li>
-        <li class="sidebar-btn u-w_sbar u-mar-b21 p-sidebar_btnline c-bk_sidebar5">
-            <p class="form c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec3/'); ?>">事業所情報</a></p>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="form c-btn_barfont2"><a href="#m-info">事業所情報</a></p>
         </li>
-        <li class="sidebar-btn u-w_sbar u-mar-b21 p-sidebar_btnline c-bk_sidebar5">
-            <p class="form c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec4/'); ?>">事業内容</a></p>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="form c-btn_barfont2"><a href="#m-business">事業内容</a></p>
         </li>
-        <li class="sidebar-btn u-w_sbar u-mar-b21 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec5/'); ?>">病院協力体制</a></p>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="info c-btn_barfont2"><a href="#m-support">病院協力体制</a></p>
         </li>
-        <li class="sidebar-btn u-w_sbar p-sidebar_btnline c-bk_sidebar5">
-            <p class="form c-btn_barfont"><a href="<?php echo home_url('/mitsuwa#sec6/'); ?>">認知症の人と家族の会</a></p>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="form c-btn_barfont2"><a href="#m-family">認知症の人と家族の会</a></p>
+        </li>
+        <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
+            <p class="form c-btn_barfont2"><a href="#m-link">外部リンク</a></p>
         </li>
     </ul>
 </aside>
