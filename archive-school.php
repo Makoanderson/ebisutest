d44e8502571b6ad4171c092e6752b837f0542db9
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-02-26 16:07:31 +0900

    feat/#26, 32, 59 ケアサロン及びスクールメニュー等スタイリング

--- archive-school.php
+++ archive-school.php
@@ -8,14 +8,14 @@
                     <picture class="u-posi-rela u-disp-flex u-justify-center">
                         <source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_mv.png" media="(min-width: 200px)" >
                         <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_mv.png" class="u-w_tabmv u-h_maxpc u-mar-mt55tab" alt="">
-                        <p class="u-posi-abso c-title_mv">宅幼老所<br><span>笑びすについて</span><br><br>可能な限り希望に沿った対応を・・・</p>
+                        <p class="u-posi-abso c-title_mv">ケアサロン笑びす<br><span>スクール紹介</span><br><br>健康な足で快適に</p>
                         <!--<p>可能な限り希望に沿った対応を・・・</p>-->
                     </picture>
                 </h1>
             </div>
 
             <!--トップへ戻る追従ボタン-->
-            <a class="c-btn-pagetop u-disp-block" href="/home/#head">
+            <a class="c-btn-pagetop u-disp-block" href="/school/#head">
                 <div class="c-btn-pagetop__arrow"></div>
                 <p>Top</p>
             </a>
@@ -25,10 +25,10 @@
 
                 <div class="p-homeebisu_content u-w_100p70p">
                     <!--ページ内リンク用サブメニュー-->
-                    <nav class="p-nav-wrap u-h_100p">
+                    <nav class="p-nav-wrap1 u-h_100p">
                         <?php wp_nav_menu(array(
-                            'theme_location' => 'yoroebisu-menu',
-                            'menu_class' => 'p-nav-yoro',
+                            'theme_location' => 'school-menu',
+                            'menu_class' => 'p-nav-school',
                             'container' => '',
                             ));?>
                     </nav>
@@ -36,12 +36,19 @@
                     <!--メインコンテンツ-->
                     <section class="u-w_100p85p u-mar-mxa p-homeebisu_post">
                         <?php if( have_posts()) : while( have_posts()) : the_post(); ?>
-                            <article class="c-card_post c-bk_post1 u-mar-my30 u-pad-p40">
-                                <h2 class="c-title_post u-disp-flex">
+                            <article class="c-card_post c-bk_post1none u-mar-my30 u-pad-p40">
+
+                            <?php
+                            $show_post_ids = array(667, 669, 671, 673, 675);
+                                
+                            if (is_archive() && in_array(get_the_ID(), $show_post_ids)) : ?>
+                                <h2 class="c-title_post u-disp-flex c-bk_title2">
                                     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_titleimg1.png" class="u-w_56">
                                     <p class="u-pad-pt3 u-pad-pl3"><?php the_title(); ?></p>
                                 </h2>
-                                <div class="u-disp-flexblocktab">
+                            <?php endif; ?>
+
+                                <div class="u-disp-flexblock">
                                     <?php the_content(); ?>
                                 </div>
                                 
@@ -53,7 +60,7 @@
                 </div>
                 
                 <!--サイドバー-->
-                <?php get_sidebar('shisetsu'); ?>
+                <?php get_sidebar('caresalon'); ?>
                 
             </div>
         </div> 
--- sidebar-caresalon.php
+++ sidebar-caresalon.php
@@ -2,31 +2,31 @@
 <aside class="sidebar-caresalon u-w_30p85p u-mar-mxa u-pad-pl6 u-pad-pt12pt0 c-bk_sidebk">
     <ul class="p-sidebar_about">
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-info">ケアサロン笑びす</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-info">ケアサロン笑びす</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-foot">フットケア</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-foot">フットケア</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-intro">施術紹介</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-intro">施術紹介</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-fee">料金表</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-fee">料金表</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="caresalon#sec1">スクール紹介</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/caresalon#sec1">スクール紹介</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="caresalon#sec1">doTERRA笑びす</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/caresalon#sec1">doTERRA笑びす</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-goods">健康グッズ</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-goods">健康グッズ</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-access">アクセスマップ</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-access">アクセスマップ</a></p>
         </li>
         <li class="sidebar-btn u-w_sbar2 u-mar-b21bk10 p-sidebar_btnline c-bk_sidebar5">
-            <p class="info c-btn_barfont2"><a href="#salon-link">外部リンク</a></p>
+            <p class="info c-btn_barfont2"><a href="/caresalon/#salon-link">外部リンク</a></p>
         </li>
     </ul>
 </aside>
