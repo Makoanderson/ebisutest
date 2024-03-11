dfb709014676db7cb8b21e877cde6bcee9faded7
anderson <yoshidamaryoshidmarad1234@gmail.com>
2024-03-11 15:38:18 +0900

    サイドバー、フッター上部周り修正

--- sidebar-shisetsu.php
+++ sidebar-shisetsu.php
@@ -1,7 +1,7 @@
 <!--施設紹介ページ用sidebar-->
-<aside class="p-sidebar-shisetsu u-w_30p85p u-mar-mxa u-pad-pl6">
-    <h3 class="p-sidebar-title c-title_sidebar"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_titleimg1.png" alt=""><p>施設紹介</p></h3>
-    <ul class="p-sidebar-wrap p-sidebar_sticky">
+<aside class="p-sidebar-shisetsu u-w_30p85p u-mar-mxa u-pad-pl6 ">
+    <h3 class="p-sidebar-title c-title_sidebar p-sidebar_sticky1"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/page-about_titleimg1.png" alt=""><p>施設紹介</p></h3>
+    <ul class="p-sidebar-wrap p-sidebar_sticky2">
         <li class="p-sidebar-btn c-bk_sidebar c-bk_sidebar1 p-sidebar-icon1 u-w_sbar u-mar-b21">
             <p class="c-btn_barfont"><a href="<?php echo home_url('/yoro/'); ?>">宅幼老所<br><span>笑びす</span></a></p>
         </li>
